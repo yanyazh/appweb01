4705ae732e81a3e2039adb304ffaa67f26cefd5d
yanyazh <97636861+yanyazh@users.noreply.github.com>
2024-10-03 08:05:30 +0000

    1_2.2

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-(function () {
-  //TODO
-})();
+function ex1_onclick() {
+  let numbers = "";
+  for (let i = 0; i <= 9; i++) {
+    numbers += i + ", ";
+  }
+  document.getElementById("ex1_content").textContent = numbers;
+}
+
+function ex2_validation() {
+  let phone = document.getElementById("ex2_text").value;
+  let pattern = /^[0-9]{9}$/;
+  const regex = /[a-zA-Z]/;
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  if (regex.test(phone)) {
+    document.getElementById("ex2_content").textContent =
+      "Phone must be a number";
+  } else if (specialChars.test(phone)) {
+    document.getElementById("ex2_content").textContent =
+      "Number can not contain special letters";
+  } else if (phone.length != 9) {
+    document.getElementById("ex2_content").textContent =
+      "Phone number must be 9 digits";
+  } else if (pattern.test(phone)) {
+    document.getElementById("ex2_content").textContent = "Valid phone number";
+  } else {
+    document.getElementById("ex2_content").textContent = "Invalid phone number";
+  }
+}
+
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drag(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text");
+  event.target.appendChild(document.getElementById(data));
+}
